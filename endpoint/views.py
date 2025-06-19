c6f8195e3a6507ddffcfd798de53a44ca45c24d5
bronkp <rorysaxton@gmail.com>
2025-06-19 13:38:32 -0700

    decoded payload status

--- endpoint/views.py
+++ endpoint/views.py
@@ -2,27 +2,31 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from endpoint.models import Device,Payload
-from rest_framework.authtoken.models import Token
 from endpoint.serializers import DeviceSerializer,PayloadSerializer
+from base64 import b64decode
+
 class DeviceViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """ 
+    permission_classes = [IsAuthenticated]
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
 class PayloadViewSet(viewsets.ModelViewSet):
     #checking for token
     permission_classes = [IsAuthenticated]
+    #When Post Request
     def create(self, request):
         data = request.data
         fCnt = data["fCnt"]
         devEUI = data["devEUI"]
+        encoded_data = data["data"]
+        
         reused = Payload.objects.filter(fCnt = fCnt).exists()
         if reused:
             return Response(status = status.HTTP_409_CONFLICT)
         else:
+            decoded = int.from_bytes(b64decode(encoded_data), 'big')
+            payload_status = True if decoded == 1 else 0
             device = Device.objects.get(devEUI=devEUI)
-            Payload.objects.create(fCnt=fCnt,status=1,device=device)
+            Payload.objects.create(fCnt=fCnt,status=payload_status,device=device)
         return Response(status=status.HTTP_202_ACCEPTED)
     queryset = Payload.objects.all()
     serializer_class = PayloadSerializer
