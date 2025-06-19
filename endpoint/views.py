181c9c30cdcb95c470317a3bef153214e0f86bdb
bronkp <rorysaxton@gmail.com>
2025-06-19 15:32:16 -0700

    device put

--- endpoint/views.py
+++ endpoint/views.py
@@ -4,9 +4,19 @@ from rest_framework.permissions import IsAuthenticated
 from endpoint.models import Device,Payload
 from endpoint.serializers import DeviceSerializer,PayloadSerializer
 from base64 import b64decode
-
+from django.db.utils import IntegrityError
 class DeviceViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
+    def create(self, request):
+        try:
+            data = request.data
+            devEUI = data['devEUI']
+            Device.objects.create(devEUI=devEUI,status=False)
+            return Response(status=status.HTTP_201_CREATED)
+        except IntegrityError:
+            return Response({"error":"id already exists"},status=status.HTTP_409_CONFLICT)
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
 class PayloadViewSet(viewsets.ModelViewSet):
@@ -18,23 +28,22 @@ class PayloadViewSet(viewsets.ModelViewSet):
             fCnt = data["fCnt"] 
             devEUI = data["devEUI"]
             encoded_data = data["data"]
+            #checking for duplicate playload, by fCnt and foreign key devEUI
             reused = Payload.objects.filter(fCnt = fCnt, device__devEUI=devEUI).exists()
             if reused:
                 return Response(status = status.HTTP_409_CONFLICT)
             else:
                 decoded = int.from_bytes(b64decode(encoded_data), 'big')
-                print(decoded)
                 payload_status = True if decoded == 1 else False
-                print(payload_status)
                 device = Device.objects.get(devEUI = devEUI)
                 device.status = payload_status
                 Payload.objects.create(fCnt=fCnt,status=payload_status,device=device)
                 device.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
+            return Response(status=status.HTTP_201_CREATED)
         except KeyError:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         except Device.DoesNotExist:
             return Response({"error":"invalid device id"},status=status.HTTP_400_BAD_REQUEST)
-            
+    # default behavior to show all payloads
     queryset = Payload.objects.all()
     serializer_class = PayloadSerializer
